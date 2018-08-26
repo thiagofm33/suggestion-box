1c693ff0bdd5fe783689f27a5aa052f56c22091e
Thiago Martins <thiagofm33@gmail.com>
2018-08-26 19:24:16 -0300

    Fixed the failing spec.

--- spec/models/suggestion_spec.rb
+++ spec/models/suggestion_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Suggestion, type: :model do
   describe :default_scope do
     context 'Trying with no Suggestions' do
       it 'does not fetch any suggestions' do
-        expect(Suggestion.count).to eq 3
+        expect(Suggestion.count).to eq 0
       end
     end
   end
